df390bc1a732ac42042ca9d964fd1e6f5b46e5cf
kimtaikee <kimtaikee@gmail.com>
2014-12-01 22:21:13 +0800

    modify extractor

--- src/gifurlextractor.cpp
+++ src/gifurlextractor.cpp
@@ -4,3 +4,24 @@ GifUrlExtractor::GifUrlExtractor(QObject *parent) :
     QObject(parent)
 {
 }
+
+GifUrlExtractor* GifUrlExtractor::instance()
+{
+    static GifUrlExtractor _instan;
+    return &_instan;
+}
+
+void GifUrlExtractor::setUrl(const QUrl& url)
+{
+    if (m_url != url) {
+        m_url = url;
+        extract();
+    }
+}
+
+void GifUrlExtractor::extract()
+{
+
+}
+
+
--- src/gifurlextractor.h
+++ src/gifurlextractor.h
@@ -2,16 +2,26 @@
 #define GIFURLEXTRACTOR_H
 
 #include <QObject>
+#include <QUrl>
 
+class Site;
 class GifUrlExtractor : public QObject
 {
     Q_OBJECT
+    Q_DISABLE_COPY(GifUrlExtractor)
 public:
-    explicit GifUrlExtractor(QObject *parent = 0);
+    static GifUrlExtractor* instance();
+    void setUrl(const QUrl& url);
 
 signals:
     void urlsReady(const QList<QString>& urls);
 
+private:
+    explicit GifUrlExtractor(QObject *parent = 0);
+    void extract();
+
+private:
+    QUrl m_url;
 };
 
 #endif // GIFURLEXTRACTOR_H
